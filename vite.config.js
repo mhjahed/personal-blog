4ad11f8ee25b86042b93503ef9b7ee7dcbf68d83
mhjahed <jah267478@gmail.com>
2026-04-22 20:09:26 +0600

    Initial commit: Personal blog project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-blog/',  // ADD THIS LINE
+  base: '/personal-blog/',  // <-- This is needed for GitHub Pages
   server: {
     port: 5173,
     strictPort: false,
